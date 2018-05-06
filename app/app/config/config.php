77566b26aacdb6a8e9236541498f015ef3217873
dossantoscarlos <anjoazrael10@gmail.com>
2018-05-06 02:39:32 -0300

    update to base_url

--- app/app/config/config.php
+++ app/app/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'https://pdv-base.herokuapp.com';
+$config['base_url'] = 'http://localhost:8900/';
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ $config['base_url'] = 'https://pdv-base.herokuapp.com';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = './../index.php';
 
 /*
 |--------------------------------------------------------------------------
